cfaea34cbc17676c68580de301dc462518adbbc5
Pedro Fernandez Diaz <pedrotik1706@gmail.com>
2026-08-22 01:43:57 +0200

    Change cache name to 'kiwi-tpv-v2' and update URLs
    Updated cache name and modified cached URLs.

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'kiwi-tpv-v1';
+const CACHE_NAME = 'kiwi-tpv-v2';
 const urlsToCache = [
-    './tpv.html',
+    './',
+    './index.html',
     './image_5ba85e.jpg',
     './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+    'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
-// Al instalar, guarda los archivos en caché
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Al navegar, sirve los archivos desde la caché si no hay internet
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => response || fetch(event.request))
